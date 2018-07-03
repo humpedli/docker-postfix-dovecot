b1090ee89fc5d62f0dae09325bfe7c741caf4b02
humpedli <hump@hump.hu>
2018-07-03 23:08:47 +0200

    run sh fixes

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 # populate environment datas into config files
 sed -i "s/myhostname =.*/myhostname = ${HOSTNAME:-localhost}/g" /etc/postfix/main.cf
-sed -i "s/myorigin =.*/myhostname = ${HOSTNAME:-localhost}/g" /etc/postfix/main.cf
+sed -i "s/myorigin =.*/myorigin = ${HOSTNAME:-localhost}/g" /etc/postfix/main.cf
 
-sed -i "s/user =.*/user = ${MYSQL_USER:-mail}/g" /etc/postfix/*.cf
-sed -i "s/password =.*/password = ${MYSQL_PASSWORD:-mail}/g" /etc/postfix/*.cf
-sed -i "s/hosts =.*/hosts = ${MYSQL_HOST:-localhost}/g" /etc/postfix/*.cf
-sed -i "s/dbname =.*/dbname = ${MYSQL_DATABASE:-mail}/g" /etc/postfix/*.cf
+find /etc/postfix/ -type f -name '*_maps.cf' | xargs sed -i "s/user =.*/user = ${MYSQL_USER:-mail}/g"
+find /etc/postfix/ -type f -name '*_maps.cf' | xargs sed -i "s/password =.*/password = ${MYSQL_PASSWORD:-mail}/g"
+find /etc/postfix/ -type f -name '*_maps.cf' | xargs sed -i "s/hosts =.*/hosts = ${MYSQL_HOST:-localhost}/g"
+find /etc/postfix/ -type f -name '*_maps.cf' | xargs sed -i "s/dbname =.*/dbname = ${MYSQL_DATABASE:-mail}/g"
 
 sed -i "s/connect =.*/connect = host=${MYSQL_HOST:-localhost} dbname=${MYSQL_DATABASE:-mail} user=${MYSQL_USER:-mail} password=${MYSQL_PASSWORD:-mail}/g" /etc/dovecot/dovecot-sql.conf.ext
 
