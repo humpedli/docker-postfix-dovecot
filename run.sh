5e350b9f350ca8906f21f81a41fb24e8260ad7e7
humpedli <hump@hump.hu>
2018-07-03 23:02:42 +0200

    More fixes

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@
 sed -i "s/myhostname =.*/myhostname = ${HOSTNAME:-localhost}/g" /etc/postfix/main.cf
 sed -i "s/myorigin =.*/myhostname = ${HOSTNAME:-localhost}/g" /etc/postfix/main.cf
 
-sed -i "s/user =.*/user = ${MYSQL_USER:-mail}/g" /etc/postfix/*
-sed -i "s/password =.*/password = ${MYSQL_PASSWORD:-mail}/g" /etc/postfix/*
-sed -i "s/hosts =.*/hosts = ${MYSQL_HOST:-localhost}/g" /etc/postfix/*
-sed -i "s/dbname =.*/dbname = ${MYSQL_DATABASE:-mail}/g" /etc/postfix/*
+sed -i "s/user =.*/user = ${MYSQL_USER:-mail}/g" /etc/postfix/*.cf
+sed -i "s/password =.*/password = ${MYSQL_PASSWORD:-mail}/g" /etc/postfix/*.cf
+sed -i "s/hosts =.*/hosts = ${MYSQL_HOST:-localhost}/g" /etc/postfix/*.cf
+sed -i "s/dbname =.*/dbname = ${MYSQL_DATABASE:-mail}/g" /etc/postfix/*.cf
 
 sed -i "s/connect =.*/connect = host=${MYSQL_HOST:-localhost} dbname=${MYSQL_DATABASE:-mail} user=${MYSQL_USER:-mail} password=${MYSQL_PASSWORD:-mail}/g" /etc/dovecot/dovecot-sql.conf.ext
 
